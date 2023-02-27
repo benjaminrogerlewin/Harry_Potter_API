defcd794c4b288e9fbc1751ddde7852ccfb7f3a4
Benjamin <benjamin.lewin@yahoo.com>
2023-02-27 16:19:47 -0500

    finished detail pages

--- harry-potter-api/src/components/OtherPage.jsx
+++ harry-potter-api/src/components/OtherPage.jsx
@@ -14,11 +14,53 @@ export default function OtherPage(props) {
         setCharacter(selectedCharacter)
     }, [])
 
-    return (
-        <div className="smallCard">
-            <div className="info">
-                <h1>{character.name}</h1>
+    if (character.image && character.dateOfBirth !== null) {
+        return (
+            <div className="smallOtherCard">
+                <div className="infoCard">
+                    <div>
+                        <img src={character.image} id="single-image" />
+                    </div>
+                    <div className="info">
+                        <h1>{character.name}</h1>
+                        <h3>House: {character.house}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                        <h3>Date of Birth: {character.dateOfBirth}</h3>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else if (character.image && character.dateOfBirth === null) {
+        return (
+            <div className="smallOtherCard">
+                <div className="infoCard">
+                    <div>
+                        <img src={character.image} id="single-image" />
+                    </div>
+                    <div className="info">
+                        <h1>{character.name}</h1>
+                        <h3>House: {character.house}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                        <h3>Date of Birth: Unknown</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="smallOtherCard">
+                <div className="infoCard">
+                    <div>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw5E3CX1jPdj1-Ohjs70JKjebmO0xe6Zkemw&usqp=CAU" id="single-image" />
+                    </div>
+                    <div className="info">
+                        <h1>{character.name}</h1>
+                        <h3>House: {character.house}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                        <h3>Date of Birth: Unknown</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
